86d077393d33c508ba219f53ffd717534099c016
Stefun <hawkd215@gmail.com>
2019-07-08 16:44:39 -0400

    still in progres

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,11 +3,33 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
+    Map<String, String> groceryMap = new HashMap<>();
+    StringBuilder stringBuilder = new StringBuilder();
+    List<Item> groceryList = new ArrayList<>();
+
+
+    String regex1 = "(\\b[;]a-z[:@^*%]a-z[;]\\b)";
+
+
+
+    public ItemParser(){}
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        Pattern p1 = Pattern.compile(regex1, Pattern.CASE_INSENSITIVE);
+        Matcher m1 = p1.matcher(valueToParse);
+
+        while(m1.find()){
+            groceryList.add();
+        }
+
+
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
